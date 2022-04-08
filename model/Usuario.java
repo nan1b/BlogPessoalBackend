229191b67dbfe552d155fa61fb1a90e02936af35
Ananda <nandabiliero@hotmail.com>
2022-04-08 12:44:45 -0300

    testes JUnit

--- model/Usuario.java
+++ model/Usuario.java
@@ -35,6 +35,20 @@ public class Usuario {
 	@OneToMany(mappedBy="usuario",cascade = CascadeType.REMOVE)
 	@JsonIgnoreProperties("usuario")
 	private List<Postagem> postagens;	
+	
+	//Metodos construtores para testes
+	// tem que estar na ordem da declaração dos atributos do usuario 
+	//construtor cheio 
+	public Usuario(Long id, String nome, String usuario, String senha, String foto){
+		this.id = id;
+		this.nome = nome;
+		this.usuario = usuario;
+		this.senha = senha;
+		this.foto = foto;
+	}
+	
+	//construtor vazio
+	public Usuario() {}
 
 	public Long getId() {
 		return id;
